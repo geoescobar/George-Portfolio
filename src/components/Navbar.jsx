b758f08d9ba0124603aa9d882cbf58e8bc82d9b6
George Escobar <geo.escobar214@gmail.com>
2022-07-18 15:18:54 -0500

    navbar complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
+
+function Navbar() {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#16161d] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo-Img" className="logo" />
+      </div>
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Skills</li>
+          <li>Contact Me</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
